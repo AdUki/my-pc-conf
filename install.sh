5abe862db632ff1279a155e4ef53787e810f09e2
Simon Mikuda <simon.mikuda@gmail.com>
2024-06-14 15:10:08 +0200

    Remove -r flag from rm command in install.sh
    Signed-off-by: Simon Mikuda <simon.mikuda@gmail.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -rf $HOME/.tmux.conf
+rm -f $HOME/.tmux.conf
 ln -s -f $PWD/tmux.conf ~/.tmux.conf
 
-rm -rf $HOME/bash_aliases
+rm -f $HOME/bash_aliases
 ln -s -f $PWD/bash_aliases ~/.bash_aliases
 
 rm -rf $HOME/.config/nvim
