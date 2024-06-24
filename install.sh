2c2d7b50b632b113e952f5e867a066a13dea9e97
Simon Mikuda <simon.mikuda@streamunlimited.com>
2024-06-24 06:13:51 +0200

    Add tig configuration file
    Signed-off-by: Simon Mikuda <simon.mikuda@streamunlimited.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 rm -f $HOME/.tmux.conf
 ln -s -f $PWD/tmux.conf ~/.tmux.conf
 
+rm -f $HOME/.tigrc
+ln -s -f $PWD/tig.conf ~/.tigrc
+
 rm -f $HOME/bash_aliases
 ln -s -f $PWD/bash_aliases ~/.bash_aliases
 
