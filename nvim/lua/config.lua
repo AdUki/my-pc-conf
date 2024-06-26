161f636e614ebca172da987578143b86243b648e
Simon Mikuda <simon.mikuda@streamunlimited.com>
2024-06-26 11:53:16 +0200

    nvim: Enable undotree instead of swapfiles
    Signed-off-by: Simon Mikuda <simon.mikuda@streamunlimited.com>

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -5,10 +5,10 @@ vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
--- vim.opt.swapfile = false
--- vim.opt.backup = false
--- vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
--- vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile = true
 
 -- vim.opt.termguicolors = true
 
