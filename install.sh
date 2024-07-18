6477297014f96b47e0de6afa0850dda7aa60b240
Simon Mikuda <simon.mikuda@gmail.com>
2024-07-18 11:03:17 +0200

    kitty: add initial configuration

--- install.sh
+++ install.sh
@@ -9,5 +9,9 @@ ln -s -f $PWD/tig.conf ~/.tigrc
 rm -f $HOME/bash_aliases
 ln -s -f $PWD/bash_aliases ~/.bash_aliases
 
+rm -f $HOME/.config/kitty/kitty.conf
+mkdir -p ~/.config/kitty
+ln -s -f $PWD/kitty.conf ~/.config/kitty/kitty.conf
+
 rm -rf $HOME/.config/nvim
 ln -s -f $PWD/nvim ~/.config/nvim
